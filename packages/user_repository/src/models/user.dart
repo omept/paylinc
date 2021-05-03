e21cb0687e2716c774bbc5907d58ed0fe40a795d
onwuasoanya_george <ndu4george@gmail.com>
2021-05-03 14:11:36 +0100

    user and auth bloc packages

--- packages/user_repository/src/models/user.dart
+++ packages/user_repository/src/models/user.dart
@@ -1,12 +1,15 @@
 import 'package:equatable/equatable.dart';
 
 class User extends Equatable {
-  const User(this.id);
+  const User(this.id, this.firstName, this.lastName, this.avatarUrl);
 
   final String id;
+  final String firstName;
+  final String lastName;
+  final String avatarUrl;
 
   @override
   List<Object> get props => [id];
 
-  static const empty = User('-');
+  static const empty = User('-', '', '', '');
 }
