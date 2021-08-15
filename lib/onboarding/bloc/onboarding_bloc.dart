a1373d6f6fe733b3ffcea6baf9b6006a3ce0db6c
onwuasoanya_george <ndu4george@gmail.com>
2021-08-15 09:40:56 +0100

    clean up

--- lib/onboarding/bloc/onboarding_bloc.dart
+++ lib/onboarding/bloc/onboarding_bloc.dart
@@ -4,7 +4,7 @@ import 'package:authentication_repository/authentication_repository.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:formz/formz.dart';
-
+import 'dart:convert';
 part 'onboarding_event.dart';
 part 'onboarding_state.dart';
 
@@ -20,18 +20,17 @@ class OnboardingBloc extends Bloc<OnboardingEvent, OnboardingState> {
   Stream<OnboardingState> mapEventToState(
     OnboardingEvent event,
   ) async* {
-    print(event);
     if (event is OnboardingRequestLogin) {
       _authenticationRepository.onboardingReqLogin();
-      yield _alreadyOnborded();
+      yield _mapAlreadyOnbordedToState();
     } else if (event is OnboardingRequestSignUp) { 
       _authenticationRepository.onboardingReqSignUp();
-      yield _alreadyOnborded();
+      yield _mapAlreadyOnbordedToState();
     }
   }
 
-  OnboardingState _alreadyOnborded() {
-      return OnboardingState(hasOnboarded: true, status: FormzStatus.pure);
+  OnboardingState _mapAlreadyOnbordedToState() {
+      return OnboardingState(hasOnboarded: true, status: FormzStatus.submissionSuccess);
   } 
 
 }
