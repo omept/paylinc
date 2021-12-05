3ea3a7a118950e61dad6f8cbcb41d870e9131207
George Onwuasoanya <georgetheprogrammer@gmail.com>
2021-12-05 09:13:23 +0100

    added mobile onboarding

--- lib/features/onboarding/view/onboarding_page.dart
+++ lib/features/onboarding/view/onboarding_page.dart
@@ -183,11 +183,7 @@ class OnboardingPage extends StatelessWidget {
   }
 
   Widget _onboardingPageMobileScreenWidget(context, constraints) {
-    return Column(children: [
-      const SizedBox(height: kSpacing * (kIsWeb ? 1 : 2)),
-      const SizedBox(height: kSpacing / 2),
-      const Divider(),
-    ]);
+    return MobileOnBoarding();
   }
 }
 
@@ -271,4 +267,210 @@ TextStyle menuItemTextStyle({required Color color}) {
     fontSize: 13.0,
     fontWeight: FontWeight.bold,
   );
+}
+
+class MobileOnBoarding extends StatefulWidget {
+  MobileOnBoarding({Key? key}) : super(key: key);
+
+  @override
+  _MobileOnBoardingState createState() => _MobileOnBoardingState();
+}
+
+class _MobileOnBoardingState extends State<MobileOnBoarding> {
+  final int _numPages = 3;
+  final PageController _pageController = PageController(initialPage: 0);
+  int _currentPage = 0;
+
+  final kTitleStyle = TextStyle(
+    color: Colors.white,
+    fontFamily: 'CM Sans Serif',
+    fontSize: 26.0,
+    height: 1.5,
+  );
+
+  final kSubtitleStyle = TextStyle(
+    color: Colors.white,
+    fontSize: 18.0,
+    height: 1.2,
+  );
+  List<Widget> _buildPageIndicator() {
+    List<Widget> list = [];
+    for (int i = 0; i < _numPages; i++) {
+      list.add(i == _currentPage ? _indicator(true) : _indicator(false));
+    }
+    return list;
+  }
+
+  Widget _indicator(bool isActive) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 150),
+      margin: EdgeInsets.symmetric(horizontal: 8.0),
+      height: 8.0,
+      width: isActive ? 24.0 : 16.0,
+      decoration: BoxDecoration(
+        color: isActive ? Colors.white : Color(0xFF7B51D3),
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+      ),
+    );
+  }
+
+  // Widget build(BuildContext context) {
+  //   return Container(
+  //     color: Colors.blue,
+  //     height: context.height,
+  //   );
+  // }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: context.height,
+      // decoration: BoxDecoration(
+      //   gradient: LinearGradient(
+      //     begin: Alignment.topCenter,
+      //     end: Alignment.bottomCenter,
+      //     stops: [0.1, 0.4, 0.7, 0.9],
+      //     colors: [
+      //       Color(0xFF3594DD),
+      //       Color(0xFF4563DB),
+      //       Color(0xFF5036D5),
+      //       Color(0xFF5B16D0),
+      //     ],
+      //   ),
+      // ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+            horizontal: kSpacing * 2, vertical: kSpacing / 3),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () => print('Skip'),
+                child: Text(
+                  'Skip',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                child: PageView(
+                  physics: ClampingScrollPhysics(),
+                  controller: _pageController,
+                  onPageChanged: (int page) {
+                    setState(() {
+                      _currentPage = page;
+                    });
+                  },
+                  children: <Widget>[
+                    _pageOneOnboarding(),
+                    _pageOneOnboarding(),
+                    _pageOneOnboarding(),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: kSpacing),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: _buildPageIndicator(),
+              ),
+            ),
+            _currentPage != _numPages - 1
+                ? Container(
+                    child: Align(
+                      alignment: FractionalOffset.bottomRight,
+                      child: TextButton(
+                        onPressed: () {
+                          _pageController.nextPage(
+                            duration: Duration(milliseconds: 500),
+                            curve: Curves.ease,
+                          );
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Text(
+                              'Next',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 22.0,
+                              ),
+                            ),
+                            SizedBox(width: 10.0),
+                            Icon(
+                              Icons.arrow_forward,
+                              color: Colors.white,
+                              size: 30.0,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                : Padding(
+                    padding: const EdgeInsets.symmetric(vertical: kSpacing * 2),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Text(
+                          'Submit',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 22.0,
+                          ),
+                        ),
+                        SizedBox(width: 10.0),
+                        Icon(
+                          Icons.arrow_forward,
+                          color: Colors.white,
+                          size: 30.0,
+                        ),
+                      ],
+                    ),
+                  ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Center _pageOneOnboarding() {
+    return Center(
+      child: Container(
+        height: 450.0,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: kSpacing * 2),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: Image(
+                    image: AssetImage(
+                      'assets/images/raster/onboarding0.png',
+                    ),
+                    height: 300.0,
+                    width: 300.0,
+                  ),
+                ),
+                SizedBox(height: 15.0),
+                Text(
+                  '0 Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+                  style: kSubtitleStyle,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
