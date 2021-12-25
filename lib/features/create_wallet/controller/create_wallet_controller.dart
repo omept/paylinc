47b45b1c0f34a3756785d6ca588f698c72d00b12
George Onwuasoanya <georgetheprogrammer@gmail.com>
2021-12-25 19:43:49 +0100

    files

---
+++ lib/features/create_wallet/controller/create_wallet_controller.dart
@@ -0,0 +1,8 @@
+import 'package:formz/formz.dart';
+import 'package:get/get.dart';
+
+class CreateWalletController extends GetxController {
+  FormzStatus _status = FormzStatus.pure;
+
+  FormzStatus get status => _status;
+}
