ae07e7fc91530ec9914a231acacb9a01af9de7b4
George Onwuasoanya <georgetheprogrammer@gmail.com>
2022-02-23 03:44:11 +0100

    WIP: added setting's route button with arrow

--- lib/features/settings/views/screens/settings_screen.dart
+++ lib/features/settings/views/screens/settings_screen.dart
@@ -23,6 +23,12 @@ part '../../controllers/settings_controller.dart';
 class SettingsScreen extends GetView<SettingsController> {
   const SettingsScreen({Key? key}) : super(key: key);
 
+  kFHeadingTextStyle(ThemeData td) => TextStyle(
+        fontSize: 14,
+        // fontWeight: FontWeight.w600,
+        color: td.textTheme.caption?.color,
+      );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -119,12 +125,109 @@ class SettingsScreen extends GetView<SettingsController> {
   }
 
   Widget _settingsMobileScreenWidget(context, constraints) {
-    return Column(children: [
-      const SizedBox(height: kSpacing * (kIsWeb ? 1 : 2)),
-      _buildHeader(onPressedMenu: () => controller.openDrawer()),
-      const SizedBox(height: kSpacing / 2),
-      const Divider(),
-    ]);
+    ThemeData td = Theme.of(context);
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: kSpacing * (kIsWeb ? 1 : 2)),
+          _buildHeader(onPressedMenu: () => controller.openDrawer()),
+          const SizedBox(height: kSpacing / 2),
+          const Divider(),
+          const SizedBox(height: kSpacing),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kSpacing),
+            child: Text("Account", style: kFHeadingTextStyle(td)),
+          ),
+          _settingsBtn(
+            iconData: EvaIcons.person,
+            data: "Profile",
+            onTap: () => null,
+          ),
+          _settingsBtn(
+            iconData: EvaIcons.npm,
+            data: "Banks",
+            onTap: () => null,
+          ),
+          const SizedBox(height: kSpacing + 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kSpacing),
+            child: Text("Security", style: kFHeadingTextStyle(td)),
+          ),
+          const SizedBox(height: kSpacing / 2.5),
+          _settingsBtn(
+            iconData: EvaIcons.lock,
+            data: "Password",
+            subData: "Last updated on: 22/12/2021",
+            subDataColor: td.textTheme.caption?.color,
+            onTap: () => null,
+          ),
+        ]);
+  }
+
+  Widget _settingsBtn(
+      {required IconData iconData,
+      required String data,
+      required Function onTap,
+      String? subData,
+      Color? subDataColor}) {
+    return SizedBox(
+      height: 50.0,
+      child: InkWell(
+        onTap: () => onTap,
+        // borderRadius: BorderRadius.circular(10),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: kSpacing + 10,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Wrap(
+                children: [
+                  _icon(iconData),
+                  const SizedBox(width: kSpacing / 2),
+                  _labelText(data,
+                      subData: subData, subDataColor: subDataColor),
+                ],
+              ),
+              Icon(EvaIcons.arrowRight)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _icon(IconData iconData) {
+    return Icon(
+      iconData,
+      size: 25,
+    );
+  }
+
+  Widget _labelText(String data, {String? subData, Color? subDataColor}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          data,
+          style: TextStyle(
+            fontWeight: FontWeight.w400,
+            letterSpacing: .8,
+            fontSize: 16,
+          ),
+        ),
+        if (subData != null)
+          Text(
+            subData,
+            style: TextStyle(
+              color: subDataColor,
+            ),
+          ),
+      ],
+    );
   }
 
   Widget _buildHeader({Function()? onPressedMenu}) {
